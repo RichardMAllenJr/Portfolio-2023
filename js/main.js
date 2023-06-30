8b6b001ce9b7a44f33761604bad74f6b0e79bec6
RichardMAllenJr <richardmallenjr1@gmail.com>
2023-06-29 20:04:22 -0500

    0629 1st updates

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     var modals = document.querySelectorAll('.modal');
     var shmodals = M.Modal.init(modals, options);
+
+    generateComets(10);
 });
 
 // Integer
@@ -41,13 +43,46 @@ function createButton(){
 //Toast
 M.toast({html: 'I am a toast!'})
 
-//Toggle Light Mode
-function toggleDark() {
-    document.body.classList.toggle('dark');
+// Multiply Number
+function multiplyByFive(number) {
+    console.log(number * 5);
+}
+multiplyByFive(100);
+
+// var product = 8 * 5;
+// console.log(product);
+
+var names = ["Hole-in-one", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Just Go Home!"]
+
+function golfScore (par, strokes) {
+    if (strokes == 1) {
+        return names[0]
+    } else if (strokes <= par - 2) {
+        return names[1]
+    } else if (strokes == par - 1 ) {
+        return names[2]
+    } else if (strokes == par) {
+        return names[3]
+    } else if (strokes == par + 1) {
+        return names[4]
+    } else if (strokes == par + 2) {
+        return names[5]
+    } else if (strokes >= par + 3) {
+        return names[6]
+    };
 }
 
-function multiplyByTwo(number) {
-    number= number * 5
+console.log(golfScore (5, 2));
+
+function toggleNav() {
+    document.getElementById('navWrapper').classList.toggle('active');
+    document.getElementById('navTrigger').classList.toggle('active');
 }
 
+function generateComets (number) {
+    //Generate Number of Comets in DOM
 
+    //Position randomly
+
+    //Activate CSS Animation randomly
+}
